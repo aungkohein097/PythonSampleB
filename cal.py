8ea178ad96b494812c8b9fdc4c8c9584a988fef9
aungkohein <koa9841@gmail.com>
2019-09-08 12:01:08 +0630

    Tax calculator

---
+++ cal.py
@@ -0,0 +1,11 @@
+width = 20
+height = 5 * 9
+vol = height * width
+print(vol)
+tax = 50 / 1000
+price = 10000
+ttax = price * tax
+round(ttax)
+ttotal = price + ttax
+round(ttotal)
+
---
+++ tax.py
@@ -0,0 +1,9 @@
+tax = 50 / 1000
+round(tax)
+price = 10000
+ttax = price * tax
+round(ttax)
+ttotal = price + ttax
+round(ttotal)
+print(ttotal)
+
