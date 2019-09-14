a256ea692df153b46d370c6c24b2a16688958d4b
aungkohein <koa9841@gmail.com>
2019-09-14 12:09:20 +0630

    List

---
+++ string.py
@@ -0,0 +1,33 @@
+#week 2A 14.9.2019
+print('"i can do everything Nothing impossible"')
+print("'i can do everything Nothing impossible'")
+
+x = "Hello world"
+print(x)
+5 * 'Hello' + 'World'
+x = 'Hello' + ' ' + 'World'
+print(x)
+a ="hello" + str(3)
+print(a)
+ int("3") + 3
+x = "programming"
+x[6]
+x[0:6]
+x[:7]
+x[8:]
+x[:]
+x[-4]
+x[5:-4]
+x[:3] + x[4:]
+x = "programming"#string
+word = [ 1, 3, 5, 7, 9, 11, 13, 15]#list
+word = [ "p", "r", "o", "g", "r", "a", "m", "m"]
+a = [1, 3, 5, 7, 9]
+b = [2, 4, 6, 8, 10]
+c = ["A", "B", "C", "D", "E"]
+x = a + b + c
+x
+x = [a, b, c]
+x
+x[1:3]
+x[1][1]
