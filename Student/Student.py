5b0342d852bfff663856d29e628c554c89737023
aungkohein <koa9841@gmail.com>
2019-09-22 12:10:41 +0630

    Loop

---
+++ Student/Student.py
@@ -0,0 +1,16 @@
+''''a = {
+	"Student" : "Mg Mg",
+	"Age" : 18,
+	"code" : 1234,
+	"Father" : "U Ba",
+	"Mother" : "Daw Mya"
+}
+b = {
+    "Student" : "Aye Aye",
+	"Age" : 17,
+	"code" : 123,
+	"Father" : "U Mya",
+	"Mother" : "Daw Ba"
+	
+}'''
+a = ['Mary','had','a','little','lamb']
---
+++ Student/result.py
@@ -0,0 +1,17 @@
+#import Student
+
+#x = Student.a["code"]
+#z = Student.a["Student"]
+#v = Student.a["Father"]
+#y = Student.b["code"]
+#c = Student.b["Student"]
+#n = Student.b["Father"]
+#print(y)
+#print(x)
+#print(z)
+#print(c)
+#print(v)
+#print(n)
+import mary
+for i in range(len(mary.a)):
+	print(i,mary.a[i])
